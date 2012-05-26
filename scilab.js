0c4ba58af248583de0b48c87d291a69a5650f4ea
Srikant <usp71084@gmail.com>
2012-05-26 12:39:21 +0530

    blocked error popups

--- scilab.js
+++ scilab.js
@@ -180,7 +180,7 @@ error=results.error;
 image=results.image;
 if (error){
         document.getElementById('message').style.visibility = 'hidden';
-	alert("Error : "+error);
+//	alert("Error : "+error);
         document.sciForm.sciresult.value=result+"\nSyntax Error : "+error;
         return;
 }
